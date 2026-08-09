b88ac5e468f281b4c4545965134e4469ab762f71
orodrigojose <contato.rodrigosilv@gmail.com>
2026-08-09 19:17:19 -0300

    fix(env): changed imported env module to correct

--- integrations/platforms/telegram/src/bot/middleware/auth.middleware.ts
+++ integrations/platforms/telegram/src/bot/middleware/auth.middleware.ts
@@ -1,9 +1,10 @@
-import { env } from "bun";
 import type { Telegraf } from "telegraf";
+import { env } from "../../config/env";
 
 export function registerAuthMiddleware(bot: Telegraf) {
   bot.use(async (ctx, next) => {
     const userId = ctx.from?.id;
+
 
     if (userId === env.USER_ID) {
       return next();
