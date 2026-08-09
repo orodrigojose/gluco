6504467e3143f30437bc7bbd834b2c04070a9ea4
orodrigojose <contato.rodrigosilv@gmail.com>
2026-08-09 19:17:33 -0300

    style: code formatting

--- integrations/platforms/telegram/src/bot/commands/index.ts
+++ integrations/platforms/telegram/src/bot/commands/index.ts
@@ -1,6 +1,8 @@
 import start from "./start.command";
 import type { Telegraf } from "telegraf";
 
-export function registerCommands(bot: Telegraf<Parameters<typeof start.fn>[0]>) {
+export function registerCommands(
+  bot: Telegraf<Parameters<typeof start.fn>[0]>,
+) {
   bot.command(start.name, start.fn);
 }
